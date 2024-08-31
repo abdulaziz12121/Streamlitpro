70d91a2b49fc38980604bd2c39374c621fea1d69
abdulaziz <abdulaziz125000f@gmail.com>
2024-08-31 17:21:43 +0300

    Abdulaziz

--- main.py
+++ main.py
@@ -48,10 +48,10 @@
 #     y_pred = model.predict(data)
 #     return {"pred": y_pred.tolist()[0]}
 
-from fastapi import FastAPI, HTTPException
-import pickle
-from pydantic import BaseModel
-from fastapi import FastAPI
+# from fastapi import FastAPI, HTTPException
+# import pickle
+# from pydantic import BaseModel
+# from fastapi import FastAPI
 import uvicorn
 import joblib
 import numpy as np
@@ -59,23 +59,23 @@ import numpy as np
 # Load the trained model
 model = joblib.load('DBSCAN_model.joblib')
 
-# FastAPI app instance
-app = FastAPI()
+# # FastAPI app instance
+# app = FastAPI()
 
-# Pydantic model for input data validation
-class InputFeatures(BaseModel):
-    yellow: float
-    red: float
-    position_encoded: int
+# # Pydantic model for input data validation
+# class InputFeatures(BaseModel):
+#     yellow: float
+#     red: float
+#     position_encoded: int
 
-# Endpoint for model prediction
-@app.post("/predict")
-async def predict(data: InputFeatures):
-    features = np.array([data.yellow, data.red, data.position_encoded]).reshape(1, -1)
-    prediction = model.fit_predict(features)
-    return {"prediction": prediction.tolist()}
+# # Endpoint for model prediction
+# @app.post("/predict")
+# async def predict(data: InputFeatures):
+#     features = np.array([data.yellow, data.red, data.position_encoded]).reshape(1, -1)
+#     prediction = model.fit_predict(features)
+#     return {"prediction": prediction.tolist()}
 
-# Run the FastAPI app
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+# # Run the FastAPI app
+# if __name__ == "__main__":
+#     import uvicorn
+    # uvicorn.run(app, host="127.0.0.1", port=8000)
